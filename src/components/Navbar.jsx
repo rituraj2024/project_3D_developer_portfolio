5d0bb25e11191e0479c5df008c7aecdd974fae4e
rituraj2024 <45riturajprasad@gmail.com>
2024-05-27 10:45:01 +0530

    msg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,114 +1,10 @@
-// import React, { useEffect, useState } from "react";
-// import { Link, redirect } from "react-router-dom";
-
-// import { styles } from "../styles";
-// import { navLinks } from "../constants";
-// import { logo, menu, close } from "../assets";
-
-// const Navbar = () => {
-//   const [active, setActive] = useState("");
-//   const [toggle, setToggle] = useState(false);
-//   const [scrolled, setScrolled] = useState(false);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       const scrollTop = window.scrollY;
-//       if (scrollTop > 100) {
-//         setScrolled(true);
-//       } else {
-//         setScrolled(false);
-//       }
-//     };
-
-//     window.addEventListener("scroll", handleScroll);
-
-//     return () => window.removeEventListener("scroll", handleScroll);
-//   }, []);
-
-//   return (
-//     <nav
-//       className={`${
-//         styles.paddingX
-//       } w-full flex items-center py-5 fixed top-0 z-20 ${
-//         scrolled ? "bg-primary" : "bg-transparent"
-//       }`}
-//     >
-//       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-//         <Link
-//           to="/"
-//           className="flex items-center gap-2"
-//           onClick={() => {
-//             setActive("");
-//             window.scrollTo(0, 0);
-//           }}
-//         >
-//           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-//           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-//             RITU RAJ &nbsp;
-//             <span className="sm:block hidden">
-//               {" "}
-//               <span classname="text-blue-600">| </span> Full Stack Developer
-//             </span>
-//           </p>
-//         </Link>
-
-//         <ul className="list-none hidden sm:flex flex-row gap-10">
-//           {navLinks.map((nav) => (
-//             <li
-//               key={nav.id}
-//               className={`${
-//                 active === nav.title ? "text-white" : "text-secondary"
-//               } hover:text-white text-[18px] font-medium cursor-pointer`}
-//               onClick={() => setActive(nav.title)}
-//             >
-//               <a href={`#${nav.id}`}>{nav.title}</a>
-//             </li>
-//           ))}
-//         </ul>
-
-//         <div className="sm:hidden flex flex-1 justify-end items-center">
-//           <img
-//             src={toggle ? close : menu}
-//             alt="menu"
-//             className="w-[28px] h-[28px] object-contain"
-//             onClick={() => setToggle(!toggle)}
-//           />
-
-//           <div
-//             className={`${
-//               !toggle ? "hidden" : "flex"
-//             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-//           >
-//             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
-//               {navLinks.map((nav) => (
-//                 <li
-//                   key={nav.id}
-//                   className={`font-poppins font-medium cursor-pointer text-[16px] ${
-//                     active === nav.title ? "text-white" : "text-secondary"
-//                   }`}
-//                   onClick={() => {
-//                     setToggle(!toggle);
-//                     setActive(nav.title);
-//                   }}
-//                 >
-//                   <a href={`#${nav.id}`}>{nav.title}</a>
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
+
+import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
-import resume from "../assets/resume/resume.pdf";
+
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
@@ -131,7 +27,9 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`w-full flex items-center py-5 fixed top-0 z-20 ${
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
         scrolled ? "bg-primary" : "bg-transparent"
       }`}
     >
@@ -149,7 +47,7 @@ const Navbar = () => {
             RITU RAJ &nbsp;
             <span className="sm:block hidden">
               {" "}
-              <span className="text-blue-600">| </span> Full Stack Developer
+              <span classname="text-blue-600">| </span> Full Stack Developer
             </span>
           </p>
         </Link>
@@ -166,12 +64,6 @@ const Navbar = () => {
               <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
-          {/* Download button */}
-          <li className="text-white  text-[18px] font-medium cursor-pointer">
-            <a href={resume} download="resume.pdf">
-              Resume
-            </a>
-          </li>
         </ul>
 
         <div className="sm:hidden flex flex-1 justify-end items-center">
@@ -202,11 +94,6 @@ const Navbar = () => {
                   <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
               ))}
-              <li className="text-white  text-[18px] font-medium cursor-pointer">
-                <a href={resume} download="resume.pdf">
-                  Resume
-                </a>
-              </li>
             </ul>
           </div>
         </div>
@@ -216,3 +103,116 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import { navLinks } from "../constants";
+// import { logo, menu, close } from "../assets";
+// import resume from "../assets/resume/resume.pdf";
+// const Navbar = () => {
+//   const [active, setActive] = useState("");
+//   const [toggle, setToggle] = useState(false);
+//   const [scrolled, setScrolled] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrollTop = window.scrollY;
+//       if (scrollTop > 100) {
+//         setScrolled(true);
+//       } else {
+//         setScrolled(false);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
+
+//   return (
+//     <nav
+//       className={`w-full flex items-center py-5 fixed top-0 z-20 ${
+//         scrolled ? "bg-primary" : "bg-transparent"
+//       }`}
+//     >
+//       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+//         <Link
+//           to="/"
+//           className="flex items-center gap-2"
+//           onClick={() => {
+//             setActive("");
+//             window.scrollTo(0, 0);
+//           }}
+//         >
+//           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+//           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+//             RITU RAJ &nbsp;
+//             <span className="sm:block hidden">
+//               {" "}
+//               <span className="text-blue-600">| </span> Full Stack Developer
+//             </span>
+//           </p>
+//         </Link>
+
+//         <ul className="list-none hidden sm:flex flex-row gap-10">
+//           {navLinks.map((nav) => (
+//             <li
+//               key={nav.id}
+//               className={`${
+//                 active === nav.title ? "text-white" : "text-secondary"
+//               } hover:text-white text-[18px] font-medium cursor-pointer`}
+//               onClick={() => setActive(nav.title)}
+//             >
+//               <a href={`#${nav.id}`}>{nav.title}</a>
+//             </li>
+//           ))}
+//           {/* Download button */}
+//           <li className="text-white  text-[18px] font-medium cursor-pointer">
+//             <a href={resume} download="resume.pdf">
+//               Resume
+//             </a>
+//           </li>
+//         </ul>
+
+//         <div className="sm:hidden flex flex-1 justify-end items-center">
+//           <img
+//             src={toggle ? close : menu}
+//             alt="menu"
+//             className="w-[28px] h-[28px] object-contain"
+//             onClick={() => setToggle(!toggle)}
+//           />
+
+//           <div
+//             className={`${
+//               !toggle ? "hidden" : "flex"
+//             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+//           >
+//             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+//               {navLinks.map((nav) => (
+//                 <li
+//                   key={nav.id}
+//                   className={`font-poppins font-medium cursor-pointer text-[16px] ${
+//                     active === nav.title ? "text-white" : "text-secondary"
+//                   }`}
+//                   onClick={() => {
+//                     setToggle(!toggle);
+//                     setActive(nav.title);
+//                   }}
+//                 >
+//                   <a href={`#${nav.id}`}>{nav.title}</a>
+//                 </li>
+//               ))}
+//               <li className="text-white  text-[18px] font-medium cursor-pointer">
+//                 <a href={resume} download="resume.pdf">
+//                   Resume
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
