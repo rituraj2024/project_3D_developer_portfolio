e3391948c1d444162c4ed7f2870de947fa7453e8
rituraj2024 <45riturajprasad@gmail.com>
2024-06-06 01:24:26 +0530

    removing resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -171,7 +171,7 @@ const Navbar = () => {
           <li className="text-secondary  text-[18px] flex flex-row gap-10 font-medium cursor-pointer">
             <a href="https://www.linkedin.com/in/riturajprasad/"> Linkedin</a>
             <a href="https://github.com/rituraj2024">Github</a>
-            <a href={rituraj_resume}>Resume</a>
+            {/* <a href={rituraj_resume}>Resume</a> */}
           </li>
         </ul>
 
@@ -207,7 +207,7 @@ const Navbar = () => {
               <li className="text-white  text-[18px] flex flex-col font-medium cursor-pointer">
                 <a href="https://github.com/rituraj2024"> Linkedin</a>
                 <a href="https://github.com/rituraj2024">Github</a>
-                <a href={rituraj_resume}>Resume</a>
+                {/* <a href={rituraj_resume}>Resume</a> */}
               </li>
             </ul>
           </div>
