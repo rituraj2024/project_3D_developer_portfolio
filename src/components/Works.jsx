7405a000c26826981abf6dc18ea5a2bb13cb3494
Rituraj <45riturajprasad@gmail.com>
2024-08-24 13:59:38 +0530

    co

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,115 @@
+// import React from "react";
+// import { Tilt } from "react-tilt";
+// import { motion } from "framer-motion";
+
+// import { styles } from "../styles";
+// import { github, youtube } from "../assets";
+// import { SectionWrapper } from "../hoc";
+// import { projects } from "../constants";
+// import { fadeIn, textVariant } from "../utils/motion";
+
+// const ProjectCard = ({
+//   index,
+//   name,
+//   description,
+//   tags,
+//   image,
+//   source_code_link_github,
+//   source_code_link_youtube,
+// }) => {
+//   return (
+//     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+//       <Tilt
+//         options={{
+//           max: 45,
+//           scale: 1,
+//           speed: 450,
+//         }}
+//         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+//       >
+//         <div className="relative w-full h-[230px]">
+//           <img
+//             src={image}
+//             alt="project_image"
+//             className="w-full h-full object-cover rounded-2xl"
+//           />
+
+//           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+//             <div
+//               onClick={() => window.open(source_code_link_github, "_blank")}
+//               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center mx-1 cursor-pointer"
+//             >
+//               <img
+//                 src={github}
+//                 alt="source code"
+//                 className="w-1/2 h-1/2 object-contain  "
+//               />
+//             </div>
+//             <div
+//               onClick={() => window.open(source_code_link_youtube, "_blank")}
+//               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+//             >
+//               <img
+//                 src={youtube}
+//                 alt="Live Link"
+//                 className="w-1/2 h-1/2 object-contain"
+//               />
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="mt-5">
+//           <h3 className="text-white font-bold text-[24px]">{name}</h3>
+//           <p className="mt-2 text-secondary text-[14px]">{description}</p>
+//         </div>
+
+//         <div className="mt-4 flex flex-wrap gap-2">
+//           {tags.map((tag) => (
+//             <p
+//               key={`${name}-${tag.name}`}
+//               className={`text-[14px] ${tag.color}`}
+//             >
+//               #{tag.name}
+//             </p>
+//           ))}
+//         </div>
+//       </Tilt>
+//     </motion.div>
+//   );
+// };
+
+// const Works = () => {
+//   return (
+//     <>
+//       <motion.div variants={textVariant()}>
+//         <p className={`${styles.sectionSubText} `}>My work</p>
+//         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+//       </motion.div>
+
+//       <div className="w-full flex">
+//         <motion.p
+//           variants={fadeIn("", "", 0.1, 1)}
+//           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+//         >
+//           Following projects showcases my skills and experience through
+//           real-world examples of my work. Each project is briefly described with
+//           links to code repositories and live demos in it. It reflects my
+//           ability to solve complex problems, work with different technologies,
+//           and manage projects effectively.
+//         </motion.p>
+//       </div>
+
+//       <div className="mt-20 flex flex-wrap gap-7">
+//         {projects.map((project, index) => (
+//           <ProjectCard key={`project-${index}`} index={index} {...project} />
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default SectionWrapper(Works, "");
+
 import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
@@ -25,7 +137,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-5 rounded-2xl sm:w-full"
       >
         <div className="relative w-full h-[230px]">
           <img
@@ -42,7 +154,7 @@ const ProjectCard = ({
               <img
                 src={github}
                 alt="source code"
-                className="w-1/2 h-1/2 object-contain  "
+                className="w-1/2 h-1/2 object-contain"
               />
             </div>
             <div
@@ -59,15 +171,17 @@ const ProjectCard = ({
         </div>
 
         <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          <h3 className="text-white font-bold text-2xl sm:text-3xl">{name}</h3>
+          <p className="mt-2 text-secondary text-base sm:text-lg">
+            {description}
+          </p>
         </div>
 
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
+              className={`text-base sm:text-lg ${tag.color}`}
             >
               #{tag.name}
             </p>
@@ -82,20 +196,20 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <p className={`${styles.sectionSubText} text-xl`}>My work</p>
+        <h2 className={`${styles.sectionHeadText} text-4xl`}>Projects</h2>
       </motion.div>
 
-      <div className="w-full flex">
+      <div className="w-full flex flex-col sm:flex-row">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          className="mt-3 text-secondary text-base sm:text-lg max-w-3xl leading-[30px]"
         >
-          Following projects showcases my skills and experience through
+          Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          links to code repositories and live demos. It reflects my ability to
+          solve complex problems, work with different technologies, and manage
+          projects effectively.
         </motion.p>
       </div>
 
